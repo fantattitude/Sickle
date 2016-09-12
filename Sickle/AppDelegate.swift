d327cf11ff9cd7b1cba15478bd52edcdc6f64e16
Vivien Leroy <vivienleroy@me.com>
2016-09-12 13:24:30 +0200

    Initial Commit

---
+++ Sickle/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Sickle
+//
+//  Created by Vivien Leroy on 12/09/2016.
+//  Copyright © 2016 Émeraude. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Sickle/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sickle
+//
+//  Created by Vivien Leroy on 12/09/2016.
+//  Copyright © 2016 Émeraude. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
